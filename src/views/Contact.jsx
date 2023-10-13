eb0c5c185c1b97d1ccb065e2e334532c4d01a637
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2023-10-12 21:50:03 -0500

    inicio de la vista contacto

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,8 +1,27 @@
 const Contact = () => {
   return (
-    <div>
-      <h1>Este es el Contact</h1>
-    </div>
+    <>
+      <section className="se-containerContact">
+        <article className="se-art__contact">
+          <div className="art-infoContact">
+            <h2>Conecta con Nosotros para Encontrar tu Auto de ensueño.</h2>
+            <p>
+              En AutoMarket, estamos dedicados a ayudarte a encontrar el
+              automóvil perfecto que se adapte a tus necesidades y deseos.
+              Nuestro equipo de expertos en automóviles está listo para
+              brindarte una experiencia excepcional desde el primer momento en
+              que nos contactes.
+            </p>
+          </div>
+          <img
+            src="https://www.autoscolombia.co/wp-content/uploads/2021/12/CONTACTANOS.jpg"
+            alt=""
+          />
+        </article>
+
+        <article></article>
+      </section>
+    </>
   );
 };
 
