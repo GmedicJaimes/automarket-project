f58c0bbd855f60d7cd429258c63144e4d92ba96e
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2023-10-14 21:32:10 -0500

    data vehiculos terminado

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,27 @@
-import { Link } from "react-router-dom";
+import data from "../data.json";
 
 const Card = () => {
   return (
     <>
       <section>
         <h1>Vehiculos del Mes</h1>
-        {}
-        <Link>
-          <button>Catalogo</button>
-        </Link>
+        {data.vehiculos.map((info) => (
+          <article key={info.id}>
+            <img src={info.src} alt="" />
+            <div>
+              <h1>{info.marca}</h1>
+              <h3>{info.tipo}</h3>
+              <span> $ {info.precio} COP</span>
+              <div>
+                <p>{info.año}</p>
+                <p>{info.kilometraje}</p>
+              </div>
+            </div>
+          </article>
+        ))}
+        <a href="#">
+          <button>Comunicate con un Asesor</button>
+        </a>
       </section>
     </>
   );
