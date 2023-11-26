d697b1654ac2a2bc0bc48632eccefd5b4a3616da
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2023-11-26 17:54:21 -0500

    seccion necesedidad en progreso

--- client/src/components/Necesidades/Necesidad.jsx
+++ client/src/components/Necesidades/Necesidad.jsx
@@ -1,48 +1,18 @@
+import "./Necesidad.css";
+
 const Necesidad = () => {
   return (
     <>
       <section className="se-containerNecesidad">
-        <h1 id="saber">Somos tu mejor opción del mercado.</h1>
+        <h1 id="saber">We are your best option on the market.</h1>
+        <p>Discover the car that matches your identity and personality</p>
         <div className="necesidad-carrusel">
-          <article className="cardNecesidad">
-            <div className="card-necesidad__info">
-              <h2>
-                ¿Estas cansado de salir con tu familia y no tener un comfort al
-                transportarse?{" "}
-              </h2>
-              <span>
-                En Automarket podemos ayudarte a escoger la mejor opción que se
-                adapte a tu necesidad.
-              </span>
-            </div>
-            <img
-              src="https://www.motor.com.co/__export/1655175867501/sites/motor/img/2022/06/13/2024-chevrolet-blazer-ev-1_crop1655175866789.jpg_242310155.jpg"
-              alt=""
-              className="image1"
-            />
-          </article>
-          <article className="cardNecesidad">
-            <img
-              src="https://www.elcarrocolombiano.com/wp-content/uploads/2020/08/20200813-CHEVROLET-ONIX-RS-TURBO-2021-PRUEBA-DE-MANEJO-VIDEO-01.jpg"
-              alt=""
-              className="image2"
-            />
-            <div className="card-necesidad__info">
-              <h2>
-                ¿Las altas temperaturas te estresan y fastidian al momento de
-                movilizarte?
-              </h2>
-              <span>
-                En Automarket contamos con buenos Autos que cuentan con un
-                excelente sistema de aire acondicionado. Sobre todo estaras
-                comodo cuando te estas transportando por la ciudad.
-              </span>
-            </div>
-          </article>
+          <article className=""></article>
+
           <article className="cardNecesidad">
             <h2>
-              En AutoMarket estamos comprometidos a escuchar <br />y cumplir lo
-              que el cliente necesite.
+              In AutoMotors, we are committed <br />
+              to listening and meeting what the customer needs.
             </h2>
           </article>
         </div>
