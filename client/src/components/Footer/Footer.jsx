c9f8a84916fc3044e2bee6750d15c8cfbaf4b6f0
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2023-12-07 14:29:59 -0500

    modificaciones en el footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,20 +1,19 @@
 import { Link } from "react-router-dom";
+import "./footer.css";
+import img from "../../assets/Screenshot_4.jpg";
 
 const Footer = () => {
   return (
     <>
       <footer className="footer">
         <section className="se-footerInfo">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQttCtth6humrgzNNUMgWpW-tWFf30Sq3AYG-BuxS1Vq_UPlNeZd78exaRZwHbPM39xG8Y&usqp=CAU"
-            alt=""
-          />
+          <img src={img} alt="" />
           <div className="texto-footer">
             <p>
-              ¡Cumple el sueño de tener vehículo propio, con AutoMarket. Somos
-              líderes en el mercado de compra y venta de vehículos en Cúcuta,
-              contamos con acompañamiento personalizado, generando experiencias
-              únicas de negocio!
+              Fulfill the dream of having your own vehicle, with AutoMotors. We
+              are leaders in the market for buying and selling high-end vehicles
+              in the United Kingdom, we have personalized support, generating a
+              unique experience when choosing your ideal car!
             </p>
           </div>
           <div className="datos-footer">
@@ -24,15 +23,15 @@ const Footer = () => {
                 src="https://cdn-icons-png.flaticon.com/128/4423/4423697.png"
                 alt=""
               />
-              +57 302 5129591
+              +44 20 5129 4591
             </p>
             <p>Av libertadores 15N- 148 local 2 y 3 urbanización Nissa</p>
           </div>
         </section>
         <section className="se-footerDerechos">
-          <p>Copyright © 2023 AutoMarket</p>
+          <p>Copyright © 2023 AutoMotors</p>
           <p>
-            Desarrollado por{" "}
+            Developed by{" "}
             <a href="https://gullianojaimes.vercel.app/" target="_blank">
               Gulliano Jaimes
             </a>
