ca5b48045fe6588211007286a38fa5dbc64c28a7
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2024-02-18 20:39:23 -0500

    arreglo tamaño banner

---
+++ client/src/components/FiltrosCards/FiltrosCards.jsx
@@ -0,0 +1,11 @@
+const Card = () => {
+  return (
+    <>
+      <section className="se-container__Autos">
+        <h1>Vehiculos del Mes</h1>
+      </section>
+    </>
+  );
+};
+
+export default Card;
