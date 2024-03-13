9624a2960adc11823b2c53cfdade655b06339f59
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2024-03-12 20:56:53 -0500

    creacion de todos a realizar

--- client/src/views/Catalogo/Catalogo.jsx
+++ client/src/views/Catalogo/Catalogo.jsx
@@ -1,11 +1,12 @@
 import Cards from "../../components/Cards/Cards";
 import BannerVenta from "../../components/BannerVenta/Venta";
 import data from "../../data.json";
+import "./Catalogo.css";
 
 const Catalogo = () => {
   return (
     <>
-      <section>
+      <section className="section-data-cards">
         <Cards datos={data} />
       </section>
       <BannerVenta />
--- client/src/views/Services/Services.jsx
+++ client/src/views/Services/Services.jsx
@@ -1,7 +1,14 @@
 import BannerVenta from "../../components/BannerVenta/Venta";
 
 const Services = () => {
-  return <></>;
+  return (
+    <>
+      <div>Banner de QAS</div>
+
+      <div>Banner de servicios que ofrecemos</div>
+      <div>Banner de imagenes tipo japones, osea 4 imagenes tip</div>
+    </>
+  );
 };
 
 export default Services;
